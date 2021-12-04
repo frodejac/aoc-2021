3a0b1b91f0186b0283ec0fa9a615c717344dab6f
Frode Jacobsen <fj@itverket.no>
2021-12-04 19:55:25 +0100

    Add solution for Day 2, Puzzle 2

--- lib/day2/day2.ex
+++ lib/day2/day2.ex
@@ -9,6 +9,14 @@ defmodule Day2 do
     end
   end
 
+  defp move({direction, delta}, {x, y, aim}) do
+    case direction do
+      "forward" -> {x + delta, y + (aim * delta), aim}
+      "up" -> {x, y, aim - delta}
+      "down" -> {x, y, aim + delta}
+    end
+  end
+
   def puzzle1() do
     File.stream!("./input/day2/input1.txt")
     |> Stream.map(&String.trim/1)
@@ -17,6 +25,18 @@ defmodule Day2 do
     |> Stream.map(&List.to_tuple/1)
     |> Enum.reduce({0, 0}, fn movement, acc -> move(movement, acc) end)
     |> Tuple.to_list
+    |> Enum.reduce(1, fn factor, acc -> factor * acc end)
+  end
+
+  def puzzle2() do
+    File.stream!("./input/day2/input1.txt")
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.split(&1, " "))
+    |> Stream.map(fn list -> [Enum.at(list, 0), String.to_integer(Enum.at(list, 1))] end)
+    |> Stream.map(&List.to_tuple/1)
+    |> Enum.reduce({0, 0, 0}, fn movement, acc -> move(movement, acc) end)
+    |> Tuple.to_list
+    |> Enum.take(2)
     |> Enum.reduce(1, fn factor, acc -> factor * acc end)
   end
 
